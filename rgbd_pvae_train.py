5a10ebbf572b51ba49ff0d02a668765cd23fb78e
Shimi Smith <smith.shimi@gmail.com>
2020-11-24 21:59:39 -0500

    pytorch vae with mse+kl loss

--- rgbd_pvae_train.py
+++ rgbd_pvae_train.py
@@ -1,27 +1,20 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as tf
+from torch import nn, optim
+import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
 
-import pyro
-import pyro.distributions as dist
-from pyro.infer import SVI, Trace_ELBO
-from pyro.optim import Adam
-
 from nyu_dataloader import setup_data_loaders
-
-pyro.set_rng_seed(101)
 
 # Input is 216*216
 class Encoder(nn.Module):
-  def __init__(self, z_dim):
+  def __init__(self, x_dim, z_dim, filters=32):
     super().__init__()
 
-    depths = [4, 32, 64, 128, 256, 512]
+    depths = [x_dim, filters, filters*2, filters*4, filters*8, filters*8]
     convs = []
     for i in range(0, len(depths)-1):
-      convs.append(nn.Sequential(nn.Conv2d(depths[i], depths[i+1], 4, padding=1, stride=2), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU()))
+      convs.append(nn.Sequential(nn.Conv2d(depths[i], depths[i+1], 4, padding=1, stride=2), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU(0.2)))
 
     self.convs = nn.Sequential(*convs)
 
@@ -33,112 +26,109 @@ class Encoder(nn.Module):
     conv_out = self.convs(x).flatten(1)
 
     mu = self.fc1(conv_out)
-    log_var = self.fc2(conv_out)
+    log_std = self.fc2(conv_out)
 
-    return mu, log_var
+    return mu, log_std
 
 
 class Decoder(nn.Module):
-  def __init__(self, z_dim):
+  def __init__(self, x_dim, z_dim, filters=32):
     super().__init__()
 
-    depths = [512, 256, 128, 64, 32, 4]
+    depths = [filters*8, filters*8, filters*4, filters*2, filters, x_dim]
 
     self.fc = nn.Linear(z_dim, depths[0]*4**2)
 
     convs = []
-    for i in range(0, len(depths)-1):
-      convs.append(nn.Sequential(nn.UpsamplingNearest2d(scale_factor=2), nn.Conv2d(depths[i], depths[i+1], 3, padding=1, padding_mode='replicate'), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU()))
+    for i in range(0, len(depths)-2):
+      convs.append(nn.Sequential(nn.UpsamplingNearest2d(scale_factor=2), nn.Conv2d(depths[i], depths[i+1], 3, padding=1, padding_mode='replicate'), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU(0.2)))
+
+    convs.append(nn.Sequential(nn.UpsamplingNearest2d(scale_factor=2), nn.Conv2d(depths[-2], depths[-1], 3, padding=1, padding_mode='replicate')))
 
     self.convs = nn.Sequential(*convs)
 
   def forward(self, z):
     fc_out = self.fc(z).view(z.shape[0], -1, 4, 4)
     conv_out = self.convs(fc_out)
-    final = torch.sigmoid(conv_out)  # 4 independent bernoulli variables per pixel
+    final = torch.tanh(conv_out)
 
     return final
 
 
 class VAE(nn.Module):
-  def __init__(self, z_dim):
+  def __init__(self, x_dim, z_dim, encoder_filters=32, decoder_filters=32):
     super().__init__()
+    self.x_dim = x_dim
     self.z_dim = z_dim
-    self.encoder = Encoder(self.z_dim)
-    self.decoder = Decoder(self.z_dim)
+    self.encoder = Encoder(self.x_dim, self.z_dim)
+    self.decoder = Decoder(self.x_dim, self.z_dim)
 
     if torch.cuda.is_available():
       self.cuda()
 
-  # p(x, z) = p(x|z)p(z)
-  def model(self, x):
-    pyro.module("decoder", self.decoder)
-    with pyro.plate("data", x.shape[0]):
-      # mean and variance of prior p(z)
-      z_mu = x.new_zeros(torch.Size((x.shape[0], self.z_dim)))
-      z_var = x.new_ones(torch.Size((x.shape[0], self.z_dim)))
+  def reparameterize(self, mu, log_std):
+    std = torch.exp(log_std)
+    eps = torch.randn_like(std)
+    return mu + eps*std
 
-      z = pyro.sample("latent", dist.Normal(z_mu, z_var).to_event(1))
-      x_means = self.decoder(z)
-      pyro.sample("obs", dist.Bernoulli(x_means).to_event(3), obs=x)
+  def forward(self, x):
+    z_mu, z_log_std = self.encoder(x)
+    z = self.reparameterize(z_mu, z_log_std)
+    return self.decoder(z), x, z_mu, z_log_std
 
-  # approximate posterior q(z|x)
-  def guide(self, x):
-    pyro.module("encoder", self.encoder)
-    with pyro.plate("data", x.shape[0]):
-      z_mu, z_log_var = self.encoder(x)
-      pyro.sample("latent", dist.Normal(z_mu, torch.exp(z_log_var)).to_event(1))
 
-  def reconstruct(self, x):
-    z_mu, z_log_var = self.encoder(x)
-    z = dist.Normal(z_mu, torch.exp(z_log_var)).sample()
-    x = self.decoder(z)
-    return x
+def neg_elbo(reconstructed, x, mu, log_std, kl_coef=1):
+    # Compute the reconstruction term - log p(x|z)
+    mse = F.mse_loss(reconstructed, x)
 
+    # -KL for gaussian case: 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    log_var = 2*log_std
+    kl = torch.mean(-0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), dim=1))
+    neg_elbo = mse + kl_coef*kl
+
+    return neg_elbo
 
 # Trains for one epoch
-def train(svi, train_loader):
+def train(vae, train_loader, optimizer):
+    vae.train()
     epoch_loss = 0
     for x in train_loader:
       if torch.cuda.is_available():
         x = x.cuda()
 
-        # compute ELBO gradient and accumulate loss
-        epoch_loss += svi.step(x)
+      optimizer.zero_grad()
+      output = vae(x)
+      loss = neg_elbo(*output)
+      epoch_loss += loss.item()
+
+      loss.backward()
+      optimizer.step()
 
     # return epoch loss
     total_epoch_loss_train = epoch_loss / len(train_loader.dataset)
     return total_epoch_loss_train
 
 
-def evaluate(svi, test_loader, use_cuda=False):
+def evaluate(vae, test_loader):
+    vae.eval()
     test_loss = 0
-    # compute the loss over the entire test set
-    for x in test_loader:
-      if torch.cuda.is_available():
+    with torch.no_grad():
+      # compute the loss over the entire test set
+      for x in test_loader:
+        if torch.cuda.is_available():
           x = x.cuda()
 
-      # compute ELBO estimate and accumulate loss
-      test_loss += svi.evaluate_loss(x)
+        output = vae(x)
+        test_loss += neg_elbo(*output, kl_coef=1.0).item()
 
     total_epoch_loss_test = test_loss / len(test_loader.dataset)
     return total_epoch_loss_test
 
 
-pyro.clear_param_store()
-# pyro.enable_validation(True)
-# pyro.distributions.enable_validation(False)
+vae = VAE(4, 400, 64, 64)
+optimizer = optim.Adam(vae.parameters(), lr=0.0005)
 
-vae = VAE(400)
-optimizer = Adam({"lr": 1e-4})
-
-# num_particles defaults to 1. Can increase to get ELBO over multiple samples of z~q(z|x).
-svi = SVI(vae.model, vae.guide, optimizer, loss=Trace_ELBO())
-
-# vae.load_state_dict(torch.load('torch_weights.save'))
-# optimizer.load('optimizer_state.save')
-
-NUM_EPOCHS = 300
+NUM_EPOCHS = 200
 TEST_FREQUENCY = 5
 BATCH_SIZE = 50
 train_loader, test_loader = setup_data_loaders(batch_size=BATCH_SIZE)
@@ -146,43 +136,41 @@ train_loader, test_loader = setup_data_loaders(batch_size=BATCH_SIZE)
 train_elbo = []
 test_elbo = []
 
-# vae.eval()
 best = float('inf')
-
-vae.train()
 
 fig, axs = plt.subplots(2, 2)
 
 for epoch in range(NUM_EPOCHS):
-    total_epoch_loss_train = train(svi, train_loader)
+    total_epoch_loss_train = train(vae, train_loader, optimizer)
     train_elbo.append(-total_epoch_loss_train)
-    print("[epoch %d]  average training loss: %.4f" % (epoch, total_epoch_loss_train))
+    print("[epoch %d]  average training loss: %.8f" % (epoch, total_epoch_loss_train))
 
     if epoch % TEST_FREQUENCY == 0:
-        vae.eval()
-        total_epoch_loss_test = evaluate(svi, test_loader)
-        vae.train()
+        total_epoch_loss_test = evaluate(vae, test_loader)
         test_elbo.append(-total_epoch_loss_test)
-        print("[epoch %d] average test loss: %.4f" % (epoch, total_epoch_loss_test))
-
-        if total_epoch_loss_test < 0:  # numerical instability occured
-          print('Negative loss occurred!!!', total_epoch_loss_test)
-          break
+        print("[epoch %d] average test loss: %.8f" % (epoch, total_epoch_loss_test))
 
         # Save stuff
         if total_epoch_loss_test < best:
           print('SAVING EPOCH', epoch)
           best = total_epoch_loss_test
-          optimizer.save('rgbd_pvae_optimizer_state.save')
-          torch.save(vae.state_dict(), 'rgbd_pvae_torch_weights.save')
+          torch.save({
+            'epoch': epoch,
+            'model_state_dict': vae.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            }, 'rgbd_checkpoint.save')
+
+        if total_epoch_loss_test < 0:  # numerical instability occured
+          print('Negative loss occurred!!!', total_epoch_loss_test)
+          break
 
         i = 0
         axs[0, 0].imshow(test_loader.dataset[i][:3].permute(1, 2, 0))
         axs[0, 1].imshow(test_loader.dataset[i][3])
 
         test_input = test_loader.dataset[i].unsqueeze(0).cuda()
-        reconstructed = vae.reconstruct(test_input).cpu().detach()[0]
+        reconstructed = vae(test_input)[0].cpu().detach()[0]
         axs[1, 0].imshow(reconstructed[:3].permute(1, 2, 0))
         axs[1, 1].imshow(reconstructed[3])
-        plt.savefig('test.png')
+        plt.savefig('rgbd_test.png')
         plt.cla()
