fix normalization

nyu_dataloader.py
@@ -33,11 +33,12 @@ def h5_loader(path):
 
 
 class NYU_DepthDataset(Dataset):
-    def __init__(self, root, transform=None):
+    def __init__(self, root, resize=None, normalize=None):
       classes = find_classes(root)
       images = get_image_paths(root, classes)
       self.images = images
-      self.transform = transform
+      self.resize = resize
+      self.normalize = normalize
 
     def __len__(self):
         return len(self.images)
@@ -47,28 +48,36 @@ class NYU_DepthDataset(Dataset):
       rgb, depth = h5_loader(path)
 
       rgb = torch.from_numpy(rgb)
-      depth = torch.from_numpy(depth)
+      depth = torch.from_numpy(depth).unsqueeze(0)
+
+      if self.resize:
+          rgb = self.resize(rgb)
+          depth = self.resize(depth)
 
       rgb_max = rgb.amax((1, 2), keepdim=True)
       rgb_min = rgb.amin((1, 2), keepdim=True)
       rgb = (rgb - rgb_min) / (rgb_max - rgb_min)
 
-      depth_max = depth.amax((0, 1), keepdim=True)
-      depth_min = depth.amin((0, 1), keepdim=True)
+      depth_max = depth.amax((1, 2), keepdim=True)
+      depth_min = depth.amin((1, 2), keepdim=True)
       depth = (depth - depth_min) / (depth_max - depth_min)
 
-      rgbd = torch.cat((rgb, depth.unsqueeze(0)))
+      rgbd = torch.cat((rgb, depth))
 
-      if self.transform:
-          rgbd = self.transform(rgbd)
+      if self.normalize:
+          rgbd = self.normalize(rgbd)
 
       return rgbd
 
 
-def setup_data_loaders(batch_size):
-  transform = transforms.Compose([transforms.Resize((128, 128)), transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))])
-  nyu_train = NYU_DepthDataset('/gruvi/usr/shimi/nyudepthv2/train', transform=transform)
-  nyu_test = NYU_DepthDataset('/gruvi/usr/shimi/nyudepthv2/val', transform=transform)
+def setup_data_loaders(batch_size, normalize=True):
+  resize = transforms.Resize((128, 128))
+  normalize = None
+  if normalize:
+      normalize = transforms.Normalize((0.5, 0.5, 0.5, 0.5), (0.5, 0.5, 0.5, 0.5))
+
+  nyu_train = NYU_DepthDataset('/gruvi/usr/shimi/nyudepthv2/train', resize=resize, normalize=normalize)
+  nyu_test = NYU_DepthDataset('/gruvi/usr/shimi/nyudepthv2/val', resize=resize, normalize=normalize)
 
   train_dloader = DataLoader(dataset=nyu_train, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=torch.cuda.is_available())
   test_dloader = DataLoader(dataset=nyu_test, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=torch.cuda.is_available())