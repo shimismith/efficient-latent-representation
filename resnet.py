45f24b71afc12df4f9c2e11772e206bd11aac6db
Shimi Smith <smith.shimi@gmail.com>
2020-12-05 23:32:47 -0500

    deeper resnet

--- resnet.py
+++ resnet.py
@@ -6,6 +6,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from resnet_nyu_dataloader import setup_data_loaders
+from torch.utils.tensorboard import SummaryWriter
+
+
+torch.cuda.set_device(1)
+
+# Deep Residual Learning for Image Recognition: https://arxiv.org/pdf/1512.03385.pdf
+class ResBlock(nn.Module):
+  def __init__(self, in_channels, out_channels):
+    super().__init__()
+
+    self.conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, 3, padding=1), nn.BatchNorm2d(out_channels), nn.ReLU(),
+                              nn.Conv2d(out_channels, out_channels, 3, padding=1), nn.BatchNorm2d(out_channels))
+
+    self.linear = None
+    if in_channels != out_channels:
+      self.linear = nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, padding=0), nn.BatchNorm2d(out_channels))
+
+  def forward(self, x):
+    residual = self.conv(x)
+    if self.linear:
+      x = self.linear(x)
+    return F.relu(residual + x)
 
 
 # Input is 128*128
@@ -14,14 +36,15 @@ class Encoder(nn.Module):
     super().__init__()
 
     self.resnet = models.resnet34(pretrained=True)
-    for param in self.resnet.parameters():
-        param.requires_grad = False
+
+    freeze = ['conv1', 'layer1', 'layer2', 'fc']
+    for name, child in self.resnet.named_children():
+      if child in freeze:
+        for param in child.parameters():
+          param.requires_grad = False
 
     self.resnet.conv1 = nn.Conv2d(4, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
     self.resnet.fc = nn.Linear(in_features=512, out_features=z_dim*2, bias=True)
-
-  def get_trainable_params():
-      return self.resnet.conv1.parameters() + self.resnet.fc.parameters()
 
   def forward(self, x):
     final = self.resnet(x)
@@ -42,9 +65,9 @@ class Decoder(nn.Module):
 
     convs = []
     for i in range(0, len(depths)-2):
-      convs.append(nn.Sequential(nn.UpsamplingNearest2d(scale_factor=2), nn.Conv2d(depths[i], depths[i+1], 3, padding=1, padding_mode='replicate'), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU()))
+      convs.append(nn.Sequential(ResBlock(depths[i], depths[i]), nn.ConvTranspose2d(depths[i], depths[i+1], 4, padding=1, stride=2), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU()))
 
-    convs.append(nn.Sequential(nn.UpsamplingNearest2d(scale_factor=2), nn.Conv2d(depths[-2], depths[-1], 3, padding=1, padding_mode='replicate')))
+    convs.append(nn.Sequential(ResBlock(depths[-2], depths[-2]), nn.ConvTranspose2d(depths[-2], depths[-1], 4, padding=1, stride=2)))
     self.convs = nn.Sequential(*convs)
 
   def forward(self, z):
@@ -126,11 +149,13 @@ def evaluate(vae, test_loader):
     return total_epoch_loss_test
 
 
+writer = SummaryWriter(log_dir='/gruvi/usr/shimi/logs')
+
 vae = VAE(400)
 model_params = [param for param in vae.parameters() if param.requires_grad]
 optimizer = optim.Adam(model_params, lr=1e-4)
 
-NUM_EPOCHS = 300
+NUM_EPOCHS = 500
 TEST_FREQUENCY = 5
 BATCH_SIZE = 50
 train_loader, test_loader = setup_data_loaders(batch_size=BATCH_SIZE, normalize=True)
@@ -142,14 +167,16 @@ best = float('inf')
 
 fig, axs = plt.subplots(2, 2)
 
-for epoch in range(NUM_EPOCHS):
+for epoch in range(1, NUM_EPOCHS+1):
     total_epoch_loss_train = train(vae, train_loader, optimizer)
     train_elbo.append(-total_epoch_loss_train)
+    writer.add_scalar('Loss/train', -total_epoch_loss_train, epoch)
     print("[epoch %d]  average training loss: %.8f" % (epoch, total_epoch_loss_train))
 
-    if (epoch+1) % TEST_FREQUENCY == 0:
+    if epoch % TEST_FREQUENCY == 0:
         total_epoch_loss_test = evaluate(vae, test_loader)
         test_elbo.append(-total_epoch_loss_test)
+        writer.add_scalar('Loss/test', -total_epoch_loss_test, epoch)
         print("[epoch %d] average test loss: %.8f" % (epoch, total_epoch_loss_test))
 
         # Save stuff
@@ -169,10 +196,10 @@ for epoch in range(NUM_EPOCHS):
         i = 0
         axs[0, 0].imshow(test_loader.dataset[i][:3].permute(1, 2, 0))
         axs[0, 1].imshow(test_loader.dataset[i][3])
-
         test_input = test_loader.dataset[i].unsqueeze(0).cuda()
         reconstructed = vae(test_input)[0].cpu().detach()[0]
         axs[1, 0].imshow(reconstructed[:3].permute(1, 2, 0))
         axs[1, 1].imshow(reconstructed[3])
-        plt.savefig('rgbd_test.png')
+        writer.add_figure('reconstruction', fig, epoch)
+        # plt.savefig('rgbd_test.png')
         plt.cla()
