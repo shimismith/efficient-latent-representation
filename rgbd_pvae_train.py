e04bb2c45efe8bee9cb3c31c2717a383ea750e23
Shimi Smith <smith.shimi@gmail.com>
2020-12-12 21:36:14 -0500

    updated model

--- rgbd_pvae_train.py
+++ rgbd_pvae_train.py
@@ -3,32 +3,35 @@ from torch import nn, optim
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
-
+from torch.distributions import Normal
 from nyu_dataloader import setup_data_loaders
+
+from nyu_dataloader_mat import setup_data_loaders
+# from nyu_dataloader import setup_data_loaders
+from torch.utils.tensorboard import SummaryWriter
+
+# torch.cuda.set_device(1)
 
 # Input is 216*216
 class Encoder(nn.Module):
-  def __init__(self, x_dim, z_dim, filters=32):
+  def __init__(self, x_dim, z_dim, filters=64):
     super().__init__()
 
     depths = [x_dim, filters, filters*2, filters*4, filters*8, filters*8]
     convs = []
     for i in range(0, len(depths)-1):
-      convs.append(nn.Sequential(nn.Conv2d(depths[i], depths[i+1], 4, padding=1, stride=2), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU(0.2)))
+        convs.append(nn.Sequential(nn.Conv2d(depths[i], depths[i+1], 4, padding=1, stride=2), nn.BatchNorm2d(depths[i+1]), nn.LeakyReLU(0.2)))
 
     self.convs = nn.Sequential(*convs)
 
-    self.fc1 = nn.Linear(depths[-1]*4**2, z_dim)
-    self.fc2 = nn.Linear(depths[-1]*4**2, z_dim)
-
+    self.fc1 = nn.Linear(depths[-1]*4**2, z_dim * 2)
 
   def forward(self, x):
     conv_out = self.convs(x).flatten(1)
-
-    mu = self.fc1(conv_out)
-    log_std = self.fc2(conv_out)
-
-    return mu, log_std
+    x = self.fc1(conv_out)
+    mean, std = torch.chunk(x, 2, dim=1)
+    std = F.softplus(std) + 1e-5
+    return Normal(loc=mean, scale=std)
 
 
 class Decoder(nn.Module):
@@ -66,27 +69,27 @@ class VAE(nn.Module):
     if torch.cuda.is_available():
       self.cuda()
 
-  def reparameterize(self, mu, log_std):
-    std = torch.exp(log_std)
-    eps = torch.randn_like(std)
-    return mu + eps*std
-
   def forward(self, x):
-    z_mu, z_log_std = self.encoder(x)
-    z = self.reparameterize(z_mu, z_log_std)
-    return self.decoder(z), x, z_mu, z_log_std
+    latent = self.encoder(x)
+    z = latent.rsample()
+    return self.decoder(z), x, latent
+
+  def reconstruct(self, x):
+      latent = self.encoder(x)
+      z = latent.rsample()
+      return self.decoder(z).mean
 
 
-def neg_elbo(reconstructed, x, mu, log_std, kl_coef=1):
-    # Compute the reconstruction term - log p(x|z)
-    mse = F.mse_loss(reconstructed, x)
+def neg_elbo(reconstructed, x, latent):
+    log_likelihood_reconstructed = reconstructed.log_prob(x).mean(dim=0).sum()
 
     # -KL for gaussian case: 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    log_var = 2*log_std
-    kl = torch.mean(-0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp(), dim=1))
-    neg_elbo = mse + kl_coef*kl
+    # kl = kl_divergence(latent, Normal(torch.zeros_like(latent.loc), torch.ones_like(latent.scale))).sum(1).mean()
+    kl = torch.mean(-0.5 * torch.sum(1 + torch.log(latent.variance) - latent.mean.pow(2) - latent.variance, dim=1))
 
-    return neg_elbo
+    elbo = log_likelihood_reconstructed - kl
+    return -elbo
+
 
 # Trains for one epoch
 def train(vae, train_loader, optimizer):
@@ -105,72 +108,55 @@ def train(vae, train_loader, optimizer):
       optimizer.step()
 
     # return epoch loss
-    total_epoch_loss_train = epoch_loss / len(train_loader.dataset)
+    total_epoch_loss_train = epoch_loss / len(train_loader)
     return total_epoch_loss_train
 
 
-def evaluate(vae, test_loader):
+def mse(vae, test_loader):
     vae.eval()
-    test_loss = 0
+    mse = 0
     with torch.no_grad():
       # compute the loss over the entire test set
       for x in test_loader:
         if torch.cuda.is_available():
           x = x.cuda()
 
-        output = vae(x)
-        test_loss += neg_elbo(*output, kl_coef=1.0).item()
+        mean = vae.reconstruct(x)
+        mse += F.mse_loss(mean, x).item()
 
-    total_epoch_loss_test = test_loss / len(test_loader.dataset)
-    return total_epoch_loss_test
+    mse = mse / len(test_loader)
+    return mse
 
+
+writer = SummaryWriter(log_dir='/gruvi/usr/shimi/logs/rgbd')
 
 vae = VAE(4, 400, 64, 64)
-optimizer = optim.Adam(vae.parameters(), lr=0.0005)
+optimizer = optim.Adam(vae.parameters(), lr=1e-4)
 
-NUM_EPOCHS = 200
+NUM_EPOCHS = 300
 TEST_FREQUENCY = 5
 BATCH_SIZE = 50
-train_loader, test_loader = setup_data_loaders(batch_size=BATCH_SIZE)
+train_loader, test_loader = setup_data_loaders(batch_size=BATCH_SIZE, normalize=True)
 
-train_elbo = []
-test_elbo = []
 
 best = float('inf')
 
 fig, axs = plt.subplots(2, 2)
 
-for epoch in range(NUM_EPOCHS):
+for epoch in range(1, NUM_EPOCHS+1):
     total_epoch_loss_train = train(vae, train_loader, optimizer)
-    train_elbo.append(-total_epoch_loss_train)
+    writer.add_scalar('Loss/train', -total_epoch_loss_train, epoch)
+    writer.add_scalar('Loss/mse', mse(vae, test_loader), epoch)
+
     print("[epoch %d]  average training loss: %.8f" % (epoch, total_epoch_loss_train))
 
     if epoch % TEST_FREQUENCY == 0:
-        total_epoch_loss_test = evaluate(vae, test_loader)
-        test_elbo.append(-total_epoch_loss_test)
-        print("[epoch %d] average test loss: %.8f" % (epoch, total_epoch_loss_test))
-
-        # Save stuff
-        if total_epoch_loss_test < best:
-          print('SAVING EPOCH', epoch)
-          best = total_epoch_loss_test
-          torch.save({
-            'epoch': epoch,
-            'model_state_dict': vae.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            }, 'rgbd_checkpoint.save')
-
-        if total_epoch_loss_test < 0:  # numerical instability occured
-          print('Negative loss occurred!!!', total_epoch_loss_test)
-          break
-
-        i = 0
-        axs[0, 0].imshow(test_loader.dataset[i][:3].permute(1, 2, 0))
-        axs[0, 1].imshow(test_loader.dataset[i][3])
-
-        test_input = test_loader.dataset[i].unsqueeze(0).cuda()
-        reconstructed = vae(test_input)[0].cpu().detach()[0]
-        axs[1, 0].imshow(reconstructed[:3].permute(1, 2, 0))
-        axs[1, 1].imshow(reconstructed[3])
-        plt.savefig('rgbd_test.png')
-        plt.cla()
+        for i in range(0, 100, 10):
+            axs[0, 0].imshow(test_loader.dataset[i][:3].permute(1, 2, 0)*0.5+0.5)
+            axs[0, 1].imshow(test_loader.dataset[i][3]*0.5+0.5)
+            test_input = test_loader.dataset[i].unsqueeze(0).cuda()
+            reconstructed = vae.reconstruct(test_input).cpu().detach()[0]
+            axs[1, 0].imshow(reconstructed[:3].permute(1, 2, 0)*0.5+0.5)
+            axs[1, 1].imshow(reconstructed[3]*0.5+0.5)
+            writer.add_figure('reconstruction{}'.format(i), fig, epoch)
+            plt.cla()
