78470873e590746739fa415e38ed53796555f0af
tssahota <73358854+tssahota@users.noreply.github.com>
2020-12-16 18:30:42 +0530

    init depth_estimation
    added depth estimation code + files

---
+++ depth_estimation/bts_dataloader.py
@@ -0,0 +1,190 @@
+# Copyright (C) 2019 Jin Han Lee
+#
+# This file is a part of BTS.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.utils.data.distributed
+from torchvision import transforms
+from PIL import Image
+import os
+import random
+
+from distributed_sampler_no_evenly_divisible import *
+
+
+def _is_pil_image(img):
+    return isinstance(img, Image.Image)
+
+
+def _is_numpy_image(img):
+    return isinstance(img, np.ndarray) and (img.ndim in {2, 3})
+
+
+def preprocessing_transforms(mode):
+    return transforms.Compose([
+        ToTensor(mode=mode)
+    ])
+
+
+class BtsDataLoader(object):
+    def __init__(self, args, mode):
+        if mode == 'test':
+            self.testing_samples = DataLoadPreprocess(args, mode, transform=preprocessing_transforms(mode))
+            self.data = DataLoader(self.testing_samples, 1, shuffle=False, num_workers=1)
+        else:
+            print('mode should be \'test\'. Got {}'.format(mode))
+            
+            
+class DataLoadPreprocess(Dataset):
+    def __init__(self, args, mode, transform=None, is_for_online_eval=False):
+        self.args = args
+        with open(args.filenames_file, 'r') as f:
+            self.filenames = f.readlines()
+    
+        self.mode = mode
+        self.transform = transform
+        self.to_tensor = ToTensor
+        self.is_for_online_eval = is_for_online_eval
+    
+    def __getitem__(self, idx):
+        sample_path = self.filenames[idx]
+        focal = float(sample_path.split()[2])
+        print("focal:", focal)
+
+        
+        data_path = self.args.data_path
+
+        image_path = os.path.join(data_path, "./" + sample_path.split()[0])
+        print("image_path:",image_path)
+        image = np.asarray(Image.open(image_path), dtype=np.float32) / 255.0
+        print("Image:",image)
+
+        if self.args.do_kb_crop is True:
+            height = image.shape[0]
+            width = image.shape[1]
+            top_margin = int(height - 352)
+            left_margin = int((width - 1216) / 2)
+            image = image[top_margin:top_margin + 352, left_margin:left_margin + 1216, :]    
+        sample = {'image': image, 'focal': focal}
+        
+        if self.transform:
+            sample = self.transform(sample)
+        
+        return sample
+    
+    def rotate_image(self, image, angle, flag=Image.BILINEAR):
+        result = image.rotate(angle, resample=flag)
+        return result
+
+    def random_crop(self, img, depth, height, width):
+        assert img.shape[0] >= height
+        assert img.shape[1] >= width
+        assert img.shape[0] == depth.shape[0]
+        assert img.shape[1] == depth.shape[1]
+        x = random.randint(0, img.shape[1] - width)
+        y = random.randint(0, img.shape[0] - height)
+        img = img[y:y + height, x:x + width, :]
+        depth = depth[y:y + height, x:x + width, :]
+        return img, depth
+
+    def train_preprocess(self, image, depth_gt):
+        # Random flipping
+        do_flip = random.random()
+        if do_flip > 0.5:
+            image = (image[:, ::-1, :]).copy()
+            depth_gt = (depth_gt[:, ::-1, :]).copy()
+    
+        # Random gamma, brightness, color augmentation
+        do_augment = random.random()
+        if do_augment > 0.5:
+            image = self.augment_image(image)
+    
+        return image, depth_gt
+    
+    def augment_image(self, image):
+        # gamma augmentation
+        gamma = random.uniform(0.9, 1.1)
+        image_aug = image ** gamma
+
+        # brightness augmentation
+        if self.args.dataset == 'nyu':
+            brightness = random.uniform(0.75, 1.25)
+
+        # color augmentation
+        colors = np.random.uniform(0.9, 1.1, size=3)
+        white = np.ones((image.shape[0], image.shape[1]))
+        color_image = np.stack([white * colors[i] for i in range(3)], axis=2)
+        image_aug *= color_image
+        image_aug = np.clip(image_aug, 0, 1)
+
+        return image_aug
+    
+    def __len__(self):
+        return len(self.filenames)
+
+
+class ToTensor(object):
+    def __init__(self, mode):
+        self.mode = mode
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    
+    def __call__(self, sample):
+        image, focal = sample['image'], sample['focal']
+        image = self.to_tensor(image)
+        image = self.normalize(image)
+
+        if self.mode == 'test':
+            return {'image': image, 'focal': focal}
+
+        depth = sample['depth']
+        if self.mode == 'train':
+            depth = self.to_tensor(depth)
+            return {'image': image, 'depth': depth, 'focal': focal}
+        else:
+            has_valid_depth = sample['has_valid_depth']
+            return {'image': image, 'depth': depth, 'focal': focal, 'has_valid_depth': has_valid_depth}
+    
+    def to_tensor(self, pic):
+        if not (_is_pil_image(pic) or _is_numpy_image(pic)):
+            raise TypeError(
+                'pic should be PIL Image or ndarray. Got {}'.format(type(pic)))
+        
+        if isinstance(pic, np.ndarray):
+            img = torch.from_numpy(pic.transpose((2, 0, 1)))
+            return img
+        
+        # handle PIL Image
+        if pic.mode == 'I':
+            img = torch.from_numpy(np.array(pic, np.int32, copy=False))
+        elif pic.mode == 'I;16':
+            img = torch.from_numpy(np.array(pic, np.int16, copy=False))
+        else:
+            img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
+        # PIL image mode: 1, L, P, I, F, RGB, YCbCr, RGBA, CMYK
+        if pic.mode == 'YCbCr':
+            nchannel = 3
+        elif pic.mode == 'I;16':
+            nchannel = 1
+        else:
+            nchannel = len(pic.mode)
+        img = img.view(pic.size[1], pic.size[0], nchannel)
+        
+        img = img.transpose(0, 1).transpose(0, 2).contiguous()
+        if isinstance(img, torch.ByteTensor):
+            return img.float()
+        else:
+            return img
